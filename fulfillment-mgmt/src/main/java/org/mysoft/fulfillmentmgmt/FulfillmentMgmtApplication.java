a12197814a0d848456c5ccacbcca3cbd52b19f10
SwamyK <swamy.kunta@gmail.com>
2019-01-08 18:37:45 +0530

    Added new micro services for fullfillment and payment management

---
+++ fulfillment-mgmt/src/main/java/org/mysoft/fulfillmentmgmt/FulfillmentMgmtApplication.java
@@ -0,0 +1,14 @@
+package org.mysoft.fulfillmentmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillmentMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulfillmentMgmtApplication.class, args);
+	}
+
+}
+
---
+++ payment-system-mgmt/src/main/java/org/mysoft/paymentsystemmgmt/PaymentSystemMgmtApplication.java
@@ -0,0 +1,14 @@
+package org.mysoft.paymentsystemmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemMgmtApplication.class, args);
+	}
+
+}
+
